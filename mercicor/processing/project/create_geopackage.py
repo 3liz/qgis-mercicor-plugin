2df4ceee70a4ebaa3b7904a187e96a839603656a
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-01-26 17:57:52 +0700

    Review the create geopackage algorithm

--- mercicor/processing/project/create_geopackage.py
+++ mercicor/processing/project/create_geopackage.py
@@ -19,6 +19,7 @@ from qgis.core import (
     QgsProcessingParameterString,
     QgsVectorFileWriter,
     QgsVectorLayer,
+    edit,
 )
 
 from mercicor.processing.project.base import BaseProjectAlgorithm
@@ -93,52 +94,98 @@ class CreateGeopackageProject(BaseProjectAlgorithm):
         base_name = self.parameterAsString(parameters, self.FILE_GPKG, context)
         project_name = self.parameterAsString(parameters, self.PROJECT_NAME, context)
         extent = self.parameterAsExtent(parameters, self.PROJECT_EXTENT, context)
+        crs = self.parameterAsCrs(parameters, self.PROJECT_CRS, context)
 
         parent_base_name = str(Path(base_name).parent)
         if not base_name.endswith('.gpkg'):
             base_name = os.path.join(parent_base_name, Path(base_name).stem + '.gpkg')
 
         if os.path.exists(base_name):
-            feedback.reportError('Le fichier existe déjà.')
+            feedback.reportError('Le fichier existe déjà. Ré-écriture du fichier…')
 
-        tables = [
-            'habitat',
-            'pression',
-            'metadata',
-        ]
-
-        geometries = {
+        tables = {
             'habitat': 'Polygon',
             'pression': 'Polygon',
             'metadata': 'None',
+            # 'liste_type_pression': 'None',
+            # 'liste_sante': 'None',
         }
 
+        self.create_geopackage(base_name, crs, tables)
+
+        output_layers = self.load_layers(base_name, feedback, tables)
+
+        # Add metadata
+        fields = output_layers['metadata'].fields()
+        feature = QgsFeature(fields)
+        feature.setAttribute(fields.indexFromName('project_name'), project_name)
+        feature.setAttribute(fields.indexFromName('crs'), str(crs.authid()))
+        feature.setAttribute(fields.indexFromName('extent'), str(extent))
+        with edit(output_layers['metadata']):
+            output_layers['metadata'].addFeature(feature)
+
+        # Load layers in the project
+        output_id = []
+        for layer in output_layers.values():
+            context.temporaryLayerStore().addMapLayer(layer)
+            context.addLayerToLoadOnCompletion(
+                layer.id(),
+                QgsProcessingContext.LayerDetails(
+                    layer.name(),
+                    context.project(),
+                    self.OUTPUT_LAYERS
+                )
+            )
+            context.project().setFileName(project_name)
+            output_id.append(layer.id())
+
+        return {self.FILE_GPKG: base_name, self.OUTPUT_LAYERS: output_id}
+
+    @staticmethod
+    def load_layers(base_name, feedback, tables):
+        """ Create vector layer object from URI. """
+        output_layers = {}
+        for table in tables.keys():
+            destination = QgsVectorLayer('{}|layername={}'.format(base_name, table), table, 'ogr')
+            if not destination.isValid():
+                raise QgsProcessingException(
+                    '* ERROR: Can\'t load layer {} in {}'.format(table, base_name))
+
+            feedback.pushInfo('The layer {} has been created'.format(table))
+            output_layers[table] = destination
+
+        return output_layers
+
+    @staticmethod
+    def create_geopackage(file_path, crs, tables) -> None:
+        """ Create the geopackage for the given"""
         encoding = 'UTF-8'
         driver_name = QgsVectorFileWriter.driverForExtension('gpkg')
-        crs = self.parameterAsCrs(parameters, self.PROJECT_CRS, context)
+        for table, geometry in tables.items():
 
-        for tab in tables:
-            # create virtual layer
-            vl_path = geometries[tab]
-            if vl_path != 'None':
-                vl_path = "{}?crs={}".format(geometries[tab], crs.authid())
-            vl = QgsVectorLayer(vl_path, tab, "memory")
-            pr = vl.dataProvider()
+            layer_path = str(geometry)
+            if layer_path != 'None':
+                layer_path += "?crs={}".format(crs.authid())
 
-            # define fields
+            vector_layer = QgsVectorLayer(layer_path, table, "memory")
+            data_provider = vector_layer.dataProvider()
+
             fields = QgsFields()
 
-            path = resources_path('data_models', '{}.csv'.format(tab))
-            csv = load_csv(tab, path)
+            path = resources_path('data_models', '{}.csv'.format(table))
+            csv = load_csv(table, path)
 
             for csv_feature in csv.getFeatures():
-                fields.append(QgsField(name=csv_feature['name'], type=int(csv_feature['type'])))
+                field = QgsField(name=csv_feature['name'], type=int(csv_feature['type']))
+                field.setComment(csv_feature['comment'])
+                field.setAlias(csv_feature['alias'])
+                fields.append(field)
 
             del csv
 
             # add fields
-            pr.addAttributes(fields)
-            vl.updateFields()  # tell the vector layer to fetch changes from the provider
+            data_provider.addAttributes(fields)
+            vector_layer.updateFields()
 
             # set create file layer options
             options = QgsVectorFileWriter.SaveVectorOptions()
@@ -146,16 +193,16 @@ class CreateGeopackageProject(BaseProjectAlgorithm):
             options.fileEncoding = encoding
 
             options.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteFile
-            if os.path.exists(base_name):
+            if os.path.exists(file_path):
                 options.actionOnExistingFile = QgsVectorFileWriter.CreateOrOverwriteLayer
 
-            options.layerName = vl.name()
+            options.layerName = vector_layer.name()
             options.layerOptions = ['FID=id']
 
             # write file
             write_result, error_message = QgsVectorFileWriter.writeAsVectorFormat(
-                vl,
-                base_name,
+                vector_layer,
+                file_path,
                 options)
 
             # result
@@ -163,39 +210,5 @@ class CreateGeopackageProject(BaseProjectAlgorithm):
                 raise QgsProcessingException('* ERROR: {}'.format(error_message))
 
             del fields
-            del pr
-            del vl
-
-        output_layers = []
-        for tab in tables:
-            dest_layer = QgsVectorLayer('{}|layername={}'.format(base_name, tab), tab, 'ogr')
-            if not dest_layer.isValid():
-                raise QgsProcessingException(
-                    '* ERROR: Can\'t load layer {1} in {0}'.format(base_name, tab))
-
-            feedback.pushInfo('The layer {} has been created'.format(tab))
-
-            if tab == 'metadata':
-                fields = dest_layer.fields()
-                feature = QgsFeature()
-                feature.setFields(fields)
-                feature.setAttribute(fields.indexFromName('project_name'), project_name)
-                feature.setAttribute(fields.indexFromName('crs'), str(crs.authid()))
-                feature.setAttribute(fields.indexFromName('extent'), str(extent))
-                dest_layer.startEditing()
-                dest_layer.addFeature(feature)
-                dest_layer.commitChanges()
-
-            output_layers.append(dest_layer.id())
-            # Ajout de la couche au projet
-            context.temporaryLayerStore().addMapLayer(dest_layer)
-            context.addLayerToLoadOnCompletion(
-                dest_layer.id(),
-                QgsProcessingContext.LayerDetails(
-                    tab,
-                    context.project(),
-                    self.OUTPUT_LAYERS
-                )
-            )
-            context.project().setFileName(project_name)
-        return {self.FILE_GPKG: base_name, self.OUTPUT_LAYERS: output_layers}
+            del data_provider
+            del vector_layer
