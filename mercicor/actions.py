26892072a60f64edbc26d46e0246de297b9ba4fd
Drillin Pierre <sio.drillin@gmail.com>
2021-04-02 18:12:47 +0200

    Actions de suppression de scenario fix #53

--- mercicor/actions.py
+++ mercicor/actions.py
@@ -4,10 +4,11 @@ __email__ = "info@3liz.org"
 
 from collections import Callable
 
-from qgis.core import Qgis, QgsAction, QgsMessageLog, QgsProject
+from qgis.core import Qgis, QgsAction, QgsExpression, QgsMessageLog, QgsProject
 from qgis.utils import iface
 
 from mercicor.definitions.relations import (
+    relations,
     scenario_pression__habitat_pression_etat_ecologique,
     scenario_pression__pression,
 )
@@ -50,6 +51,47 @@ def change_scenario(*args, project: QgsProject = None):
     )
 
 
+def delete_scenario(*args, project: QgsProject = None):
+    """ Action used to delete the scenario and his entities child """
+    scenario_id = int(args[0])
+    scenario_name = args[1]
+    scenario_layer = None
+
+    if project is None:
+        project = QgsProject.instance()
+
+    for relation in relations:
+        if relation.referenced_layer == 'scenario_pression':
+            relation = project.relationManager().relation(relation.qgis_id)
+            if not relation.isValid():
+                QgsMessageLog.logMessage(
+                    'Impossible de trouver la relation {}, est-ce qu\'elle est présente '
+                    'avec un autre nom ?'.format(relation.qgis_id),
+                    'Mercicor',
+                    Qgis.Warning
+                )
+                continue
+            scenario_layer = relation.referencedLayer()
+            layer = relation.referencingLayer()
+            layer.startEditing()
+            filter_expression = QgsExpression.createFieldEqualityExpression('scenario_id', scenario_id)
+            for feature in layer.getFeatures(filter_expression):
+                layer.deleteFeature(feature.id())
+            layer.commitChanges()
+
+    scenario_layer = QgsProject.instance().mapLayersByName(scenario_layer.name())[0]
+    scenario_layer.startEditing()
+    filter_expression = QgsExpression.createFieldEqualityExpression('id', scenario_id)
+    for feature in scenario_layer.getFeatures(filter_expression):
+        scenario_layer.deleteFeature(feature.id())
+    scenario_layer.commitChanges()
+
+    iface.messageBar().pushSuccess(
+        'Mercicor',
+        'Suppression du scénario {}, ID {}'.format(scenario_name, scenario_id)
+    )
+
+
 action_change_scenario = QgsAction(
     QgsAction.GenericPython,
     'Mettre ce scénario par défaut',
@@ -57,6 +99,17 @@ action_change_scenario = QgsAction(
     '',
     False,
     'Permet de changer de scénario pour les couches pression et habitat écologique',
+    ['Feature', 'Field'],
+    ''
+)
+
+action_delete_scenario = QgsAction(
+    QgsAction.GenericPython,
+    'Supprimer ce scénario',
+    CALL.format(action_name=delete_scenario.__name__, params='[% "id" %], \'[% "nom" %]\''),
+    '',
+    False,
+    'Permet de supprimer le scénario en supprimant les entités enfants',
     ['Feature', 'Field'],
     ''
 )
@@ -77,5 +130,11 @@ actions_list = {
         2,
         change_scenario,
         action_change_scenario
+    ),
+    delete_scenario.__name__: Action(
+        'scenario_pression',
+        2,
+        delete_scenario,
+        action_delete_scenario
     )
 }
