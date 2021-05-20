ba5765eade4ca266889a991e0632fd9560b6ecdd
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-05-20 10:28:13 +0700

    Docker - Give rights back to the users, for QML

--- .docker/exec_tests.sh
+++ .docker/exec_tests.sh
@@ -5,3 +5,6 @@ TEST_PATTERN=${1:default_pattern}
 
 docker exec -t qgis sh \
   -c "export TEST_PATTERN=${TEST_PATTERN} && cd /tests_directory/${PLUGIN_NAME} && qgis_testrunner.sh tests.test_runner.test_package"
+
+docker exec -t qgis sh \
+  -c "chown -R `stat -c "%u:%g" ../${PLUGIN_NAME}` /tests_directory/${PLUGIN_NAME}"
